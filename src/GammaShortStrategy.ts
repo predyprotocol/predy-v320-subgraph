b91db07cd99230a8a97db46239259058ddde292a
syuhei176 <syuhei176@gmail.com>
2023-02-25 04:58:51 +0900

    update

---
+++ src/GammaShortStrategy.ts
@@ -0,0 +1,26 @@
+import { DepositedToStrategy, WithdrawnFromStrategy } from '../generated/GammaShortStrategy/GammaShortStrategy'
+import { StrategyUserHistoryItem } from '../generated/schema'
+
+export function handleDepositedToStrategy(event: DepositedToStrategy): void {
+  const entity = new StrategyUserHistoryItem(event.transaction.hash.toHex())
+
+  entity.action = 'DEPOSIT'
+  entity.account = event.params.account
+  entity.strategyAmount = event.params.strategyTokenAmount
+  entity.marginAmount = event.params.depositedAmount
+
+  entity.createdAt = event.block.timestamp
+  entity.save()
+}
+
+export function handleWithdrawnFromStrategy(event: WithdrawnFromStrategy): void {
+  const entity = new StrategyUserHistoryItem(event.transaction.hash.toHex())
+
+  entity.action = 'WITHDRAW'
+  entity.account = event.params.account
+  entity.strategyAmount = event.params.strategyTokenAmount
+  entity.marginAmount = event.params.withdrawnAmount
+
+  entity.createdAt = event.block.timestamp
+  entity.save()
+}
